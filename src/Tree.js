4845ef06e89163c5d62ec7bd9ce94f234f795632
Bernardo Kropiwiec <bernardo.krop@hotmail.com>
2021-03-25 00:41:10 -0400

    Começo das funçoes para animação

--- src/Tree.js
+++ src/Tree.js
@@ -5,6 +5,10 @@ class Tree{
         this.x = x
         this.y = y
 
+        this.running = false;
+        this.timeout = null; 
+        this.node = null;    
+
         this.backgoundColor = "#bdfcfa"
 
         this.draw()
@@ -63,5 +67,47 @@ class Tree{
         this.draw();
     }
 //--------Animação-----------
+    startAnimation(frame,...args){
+        if(this.running)
+            throw Error('Animação em progresso')
+        else{
+            this.running = true
+            this.node = this.root
 
+            this.resetVisuals()
+            this.continueAnimation(frame.bind(this),...args)
+        }
+    }
+    continueAnimation(frame,...args){
+        this.timeout = setTimeout(()=>frame.bind(this)(...args),
+            this.controls.animationInterval)
+    }
+    stopAnimation(comlpete = ()=>{},...callbackArgs){
+        this.running = false
+        this.node = null
+        clearTimeout(this.timeout)
+        setTimeout(()=> comlpete(...callbackArgs),this.controls.animationInterval)
+    }
+    addValueVisual(value,comlpete = ()=>{},...callbackArgs){
+        this.startAnimation(this.addValueFrame,value,comlpete,...callbackArgs)
+    }
+    addValueFrame(value,complete,...callbackArgs){
+        if(!this.node.isFilled()){
+            this.insert(value)
+            this.node.paint(Node_.SUCCESS)
+            this.draw()
+            this.stopAnimation(complete,...callbackArgs)
+        }else{
+            this.node.paint(Node_.VISITED)
+            this.updateDrawing()
+
+            if(value < this.node.data)
+                this.node = this.node.left
+            else if(vaue > this.node.data)
+                this.node = this.node.right
+
+            this.continueAnimation(this.addValueFrame,value,complete,...callbackArgs)
+        }
+
+    }
 }
